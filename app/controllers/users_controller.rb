16fa28a414227f387d543b27e548b75eff70fffa
Robert Gott <robgott@yahoo.com>
2017-04-22 14:51:38 -0400

    view group and role
    group and role will be preselected by inviter
    confirm email will be checked with a email confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.joins(:group).joins(:role).all
   end
 
   # GET /users/1
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.joins(:group).joins(:role).where("user.id = #{params[:id]}")
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
